021bf4d92a0c4630452075c14695b47ae7bc22b4
molesen <molesen@student.hive.fi>
2022-11-14 13:58:55 +0200

    made animal non instantianable

--- cpp4/ex01/Animal.hpp
+++ cpp4/ex01/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:07:04 by molesen           #+#    #+#             */
-/*   Updated: 2022/11/14 13:33:12 by molesen          ###   ########.fr       */
+/*   Updated: 2022/11/14 13:57:44 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ class Animal
 {
 protected:
 	std::string type;
-public:
 	Animal();
+public:
 	virtual ~Animal();
 	Animal(const Animal& t);
 	Animal&	operator=(const Animal& t);
--- cpp4/ex01/main.cpp
+++ cpp4/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:05:56 by molesen           #+#    #+#             */
-/*   Updated: 2022/11/14 13:25:42 by molesen          ###   ########.fr       */
+/*   Updated: 2022/11/14 13:58:05 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int	main(void)
 {
-	const	Animal* meta = new Animal();
 	const	Animal* j = new Dog();
 	const	Animal* i = new Cat();
 	//const	Animal* testDog[3] = {new Dog(), new Dog(), new Dog()};
@@ -24,10 +23,8 @@ int	main(void)
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
-	meta->makeSound();
 	delete j;//should not create a leak
 	delete i;
-	delete meta;
 	//delete[] testDog;
 	return 0;
 }
