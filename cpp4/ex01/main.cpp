f03a9462f272e7305f11e0bb5917dcffc0bdd4f5
molesen <molesen@student.hive.fi>
2022-11-14 16:03:49 +0200

    made array of cats and dogs in animal array

--- cpp4/ex01/main.cpp
+++ cpp4/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:05:56 by molesen           #+#    #+#             */
-/*   Updated: 2022/11/14 13:58:05 by molesen          ###   ########.fr       */
+/*   Updated: 2022/11/14 16:03:19 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(void)
 {
 	const	Animal* j = new Dog();
 	const	Animal* i = new Cat();
-	//const	Animal* testDog[3] = {new Dog(), new Dog(), new Dog()};
+	const	Animal* testAnimal[10];
+	int		indx;
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
@@ -25,6 +26,20 @@ int	main(void)
 	j->makeSound();
 	delete j;//should not create a leak
 	delete i;
-	//delete[] testDog;
+	indx = 0;
+	std::cout << "create cats and dogs in array\n";
+	while (indx < 10)
+	{
+		if (indx >= 5)
+			testAnimal[indx++] = new Cat();
+		else
+			testAnimal[indx++] = new Dog();
+	}
+	indx = 0;
+	std::cout << "destroy cats and dogs in array\n";
+	while (indx < 10)
+	{
+		delete testAnimal[indx++];
+	}
 	return 0;
 }
