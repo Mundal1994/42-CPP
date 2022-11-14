b9db3a9e1e235f50024c0d6c11f058d014634933
molesen <molesen@student.hive.fi>
2022-11-14 11:43:03 +0200

    finished ex00 in cpp4

---
+++ cpp4/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 11:05:56 by molesen           #+#    #+#             */
+/*   Updated: 2022/11/14 11:42:30 by molesen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main(void)
+{
+	const	Animal* meta = new Animal();
+	const	Animal* j = new Dog();
+	const	Animal* i = new Cat();
+	
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	return 0;
+}
