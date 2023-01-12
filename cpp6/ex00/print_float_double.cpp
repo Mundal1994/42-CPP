e06f587d13205eb4613afb1567b4adb00cc6155c
molesen <molesen@student.hive.fi>
2023-01-12 16:04:51 +0200

    fixed bugs in printing of float and double

--- cpp6/ex00/print_float_double.cpp
+++ cpp6/ex00/print_float_double.cpp
@@ -6,17 +6,19 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:00:26 by molesen           #+#    #+#             */
-/*   Updated: 2023/01/11 16:06:58 by molesen          ###   ########.fr       */
+/*   Updated: 2023/01/12 16:04:07 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "converter.hpp"
 
-static int	calc_float_precision(char* input)
+static int	calc_float_precision(char* input, int type)
 {
 	int	i;
 	int	count;
 
+	if (type == INT || type == CHAR)
+		return (1);
 	i = ft_strlen_stop(input, strlen(input), '.') + 1;
 	count = 0;
 	while (input[i] != '\0' && input[i] != 'f')
@@ -37,15 +39,21 @@ static void	print_label(int f)
 
 void	analyse_float_double(char* input, int type, int f)
 {
-	std::cout.precision(calc_float_precision(input));
+	std::cout.precision(calc_float_precision(input, type));
 	print_label(f);
 	switch (type)
 	{
 		case CHAR:
-			std::cout << (float)input[0] << ".0f" << std::endl;
+			std::cout << (float)input[0];
+			if (f == FLOAT)
+				std::cout << "f";
+			std::cout << std::endl;
 			break;
 		case INT:
-			std::cout << atoi(input) << ".0f" << std::endl;
+			std::cout << atoi(input) << ".0";
+			if (f == FLOAT)
+				std::cout << "f";
+			std::cout << std::endl;
 			break;
 		case FLOAT:
 		case DOUBLE:
