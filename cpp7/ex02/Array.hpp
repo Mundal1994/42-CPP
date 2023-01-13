134fb26d161b594c431de72ad267b54e784db781
molesen <molesen@student.hive.fi>
2023-01-13 13:10:29 +0200

    working progress

--- cpp7/ex02/Array.hpp
+++ cpp7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:07:12 by molesen           #+#    #+#             */
-/*   Updated: 2023/01/12 16:49:08 by molesen          ###   ########.fr       */
+/*   Updated: 2023/01/13 13:08:47 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,66 @@
 
 #include <iostream>
 
-template <class T>
+template< typename T>
 class	Array
 {
 private:
+	unsigned int size;
+	T* array;
 public:
-	Array();
+	Array() { array = new T[1]; };
+	Array(unsigned int n) {
+		size = n;
+		array = new T[n];
+	};
+	~Array() {
+		std::cout << "destructor" << std::endl;
+		return ;
+	};
+	Array(Array<T>& t) {//removed const
+		*this = t;
+		
+		/*
+		unsigned int	i;
+		unsigned int	size;
+
+		size = t.getSize();
+		(*this).array = new T(size);
+		std::cout << size << t.getElem(0) << std::endl;
+		i = 0;
+		while (i < size)
+		{
+			(*this).setArray(i, t.getElem(i));
+			++i;
+		}*/
+		//think it has to be new t (or something like that)
+	};
+	Array&	operator=(Array<T>& t) {//removed const
+		if (this != &t)
+		{
+			(*this).array = t.array;
+			(*this).size = t.size;
+		}
+		return (*this);
+	};
+	unsigned int	getSize(void) { return (size); }
+	T	getElem(int elem) { return ((*this).array[elem]); }//removed const
+	void	setArray(int elem, T value)
+	{
+		(*this).array[elem] = value;
+	}
+	void	getArray(void)
+	{
+		unsigned int	i;
+
+		i = 0;
+		while (i < (*this).getSize())
+		{
+			std::cout << (*this).array[i] << "->";
+			++i;
+		}
+		std::cout << std::endl;
+	}
 };
 
 #endif
--- cpp7/ex02/main.cpp
+++ cpp7/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: molesen <molesen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 12:12:04 by molesen           #+#    #+#             */
-/*   Updated: 2023/01/12 16:36:34 by molesen          ###   ########.fr       */
+/*   Updated: 2023/01/13 12:51:38 by molesen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int	main(void)
 {
-	int a[] = {1, 2, 4, 8, 16};
+	//int * a = new int();
+
+	Array<int> here(5);
 	
+	here.setArray(0, 0);
+	here.setArray(1, 1);
+	here.setArray(2, 2);
+	here.setArray(3, 3);
+	here.setArray(4, 4);
+	here.getArray();
+	
+	Array<int> newnew(here);
+	here.setArray(4, 5);
+
+	std::cout << "orig: ";
+	here.getArray();
+	std::cout << "new: ";
+	newnew.getArray();
 	return (0);
 }
